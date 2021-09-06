beddcd60618d29eeb5e0ab25f7a539a6ee88a93e
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-09-06 18:58:11 +0100

    clean up server index.js

--- server/index.js
+++ server/index.js
@@ -7,12 +7,6 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const app = express();
 
-
-//NOTE:
-  //   - Test as many weird cases as you can, try to break your code and defensively program to ensure it can deal
-  //   - Add password, salt and hash -> better level of (internal) encryption, think about when and how to do this to ensure
-  //     safety when passing props around etc.
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(cors());
@@ -51,7 +45,6 @@ app.post("/login", function(req, res) {
           res.sendStatus(401);
         }
       });
-      // return res.send(foundList._id)
      }
     else if (!foundList) res.sendStatus(401);
     else res.send(err)
@@ -79,7 +72,7 @@ app.post("/register", function(req, res) {
   });
 });
 
-// Define RESTful API For User Route
+// User Routes
 app.route("/notes/:userId")
 .get(function(req, res) {
   console.log("GET request " + new Date());             //Log GET requests to make sure no memory leak
@@ -104,89 +97,6 @@ app.delete("/notes/:userId/:noteId", function(req, res) {
     else res.send(err);
   } )
 })
-
-
-
-// Define RESTful API for root route
-
-///////////////////// Requests Targeting All Routes
-app.route("/")
-.get(function(req,res){
-  console.log("GET request" + new Date());
-  Note.find(function(err, foundNotes) {
-    if (!err) {
-      res.send(foundNotes);
-    } else {
-      res.send(err);
-    }
-
-  });
-})
-.post(function(req, res) {
-  const newNote = new Note({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newNote.save();
-  res.redirect("/");
-})
-.delete(function(req, res) {
-  Note.deleteMany(function(err){
-    if (!err) {
-      res.redirect("/");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-// /////////////// Requests Targeting Specific Routes
-
-// app.route("/:noteTitle")
-// .get(function(req, res) {
-//   Note.findOne({title: req.params.noteTitle}, function(err, foundNote) {
-//     if (foundNote) {
-//       res.send(foundNote)
-//     } else {
-//       res.send("No note matching that title was found.");
-//     }
-//   })
-// })
-// .put(function(req, res) {
-//   Note.update(
-//     {title: req.params.noteTitle},
-//     {title: req.body.title, content: req.body.content},
-//     {overwrite: true},
-//     function(err) {
-//       res.redirect("/");
-//     }
-//   )
-// })
-// .patch(function(req, res) {
-//   Note.update(
-//     {title: req.params.noteTitle},
-//     {$set: req.body},
-//     function(err) {
-//       if (!err) {
-//         res.send("Successfully updated note");
-//       } else {
-//         res.send(err);
-//       }
-//     }
-//   )
-// })
-// .delete(function(req, res) {
-//   Note.deleteOne(
-//     {title: req.params.noteTitle},
-//     function(err) {
-//       if (!err) {
-//         res.send("Successfully deleted note");
-//       } else {
-//         res.send(err);
-//       }
-//     }
-//   )
-// });
 
 // Return HTTP Server Object
 const port = 8000;
